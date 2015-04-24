a504c67ec597f519f6b45a22bc7b49b0802ac2c1
Yuci Zhou <yucizhou97@gmail.com>
2015-04-24 15:46:17 +0800

    Rails Homework

---
+++ rails-practice/app/controllers/posts_controller.rb
@@ -0,0 +1,88 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :reply]
+  before_action :authenticate_user!
+  # paginates_per 10
+  load_and_authorize_resource
+
+  # GET /posts
+  def index
+    @posts = Post.all.includes(:replies)
+    @pages = @posts.page(params[:page]).per(10)
+    respond_to do |format|  
+      format.html
+      format.json do
+        render json: @posts
+      end
+    end
+
+
+  end
+
+  def abc
+    @posts = Post.hottest
+    render :index
+  end
+
+
+  # GET /posts/1
+  def show
+    @post.Clicks +=1
+    @post.save
+    @pages = @post.replies.page(params[:page]).per(10)
+  end
+
+  def reply
+    reply = @post.replies.create(params[:reply].permit(:body))
+    reply.author = current_user
+    reply.save
+    redirect_to :back, notice: "reply done"
+  end
+
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
---
+++ rails-practice/app/models/reply.rb
@@ -0,0 +1,15 @@
+class Reply
+	include Mongoid::Document
+	include Mongoid::Timestamps
+
+	field :body, type: String, default: ""
+	belongs_to :post
+
+	validates_presence_of :post
+
+	after_create do 
+		post.inc(:replies_count => 1)
+	end
+
+	belongs_to :author, class_name: "User"
+end
